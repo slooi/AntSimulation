63237c22d1c0d156400eb041658c931f7e5dcc99
slooi <s.looi2@outlook.com>
2021-04-12 05:05:50 +1200

    add in testing

--- src/client/Game.ts
+++ src/client/Game.ts
@@ -15,8 +15,19 @@ export default class Game {
 
     loopInitialiser() {
         const loop = () => {
-            this.renderer.resetBuffer(this.objectManager.getNumberOfCells());
-            console.log(10);
+            this.renderer.clear();
+            if (0) {
+                this.renderer.resetBuffer(this.objectManager.getNumberOfCells());
+                this.objectManager.update();
+                this.objectManager.render();
+            } else {
+                this.renderer.resetBuffer(1);
+                this.renderer.addData(0, 0, 2, 50, 0, 10);
+                // this.renderer.addData(0, 50, 0, 255, 0, 10);
+                // this.renderer.addData(50, 50, 0, 0, 255, 10);
+            }
+            this.renderer.render();
+            console.log("tick");
             // requestAnimationFrame(loop);
         };
         // loop();
