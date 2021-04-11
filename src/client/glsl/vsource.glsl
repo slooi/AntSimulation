b078bc5bf179d5fd05ffbb1a4580d567d42ad488
slooi <s.looi2@outlook.com>
2021-04-12 06:01:14 +1200

    add new uniform

--- src/client/glsl/vsource.glsl
+++ src/client/glsl/vsource.glsl
@@ -4,9 +4,10 @@ attribute vec4 a_ColorNSize;
 varying vec4 v_ColorNSize;
 
 uniform float u_DiaSize;
+uniform float u_CanvasHalfSize;
 
 void main(){
 	v_ColorNSize =a_ColorNSize;
 	gl_PointSize = u_DiaSize;
-	gl_Position = vec4(a_Position/250.0,0,1);
+	gl_Position = vec4(a_Position/u_CanvasHalfSize,0,1);
 }
