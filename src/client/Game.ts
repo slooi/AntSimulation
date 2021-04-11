ea0a93d47021d57eba5f8ef0f4b9f536c3154044
slooi <s.looi2@outlook.com>
2021-04-12 06:00:13 +1200

    Change game loop

--- src/client/Game.ts
+++ src/client/Game.ts
@@ -14,23 +14,31 @@ export default class Game {
     }
 
     loopInitialiser() {
+        let oldDate = new Date();
         const loop = () => {
-            this.renderer.clear();
-            if (0) {
-                this.renderer.resetBuffer(this.objectManager.getNumberOfCells());
-                this.objectManager.update();
-                this.objectManager.render();
-            } else {
-                this.renderer.resetBuffer(1);
-                this.renderer.addData(0, 0, 2, 50, 0, 10);
-                // this.renderer.addData(0, 50, 0, 255, 0, 10);
-                // this.renderer.addData(50, 50, 0, 0, 255, 10);
+            if (
+                ((new Date() as unknown) as number) - ((oldDate as unknown) as number) >
+                1000 / 30
+            ) {
+                this.renderer.clear();
+                if (1) {
+                    this.renderer.resetBuffer(this.objectManager.getNumberOfCells());
+                    this.objectManager.update();
+                    this.objectManager.render();
+                } else {
+                    this.renderer.resetBuffer(1);
+                    this.renderer.addData(0, 0, 2, 50, 0, 10);
+                    // this.renderer.addData(0, 50, 0, 255, 0, 10);
+                    // this.renderer.addData(50, 50, 0, 0, 255, 10);
+                }
+                this.renderer.render();
+                console.log("tick");
+
+                oldDate = new Date();
             }
-            this.renderer.render();
-            console.log("tick");
-            // requestAnimationFrame(loop);
+            requestAnimationFrame(loop);
         };
-        // loop();
-        setInterval(loop, 100);
+        loop();
+        // setInterval(loop, 25);
     }
 }
