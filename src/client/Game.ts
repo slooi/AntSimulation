203656b11d9af28330b97c1257e1479e177f5aef
slooi <s.looi2@outlook.com>
2021-04-12 00:17:01 +1200

    Pass correct args

--- src/client/Game.ts
+++ src/client/Game.ts
@@ -5,7 +5,7 @@ export default class Game {
     renderer: Renderer;
     objectManager: ObjectManager;
     constructor({ canvas }: GameDependencies) {
-        this.objectManager = new ObjectManager();
+        this.objectManager = new ObjectManager({ width: canvas.width, height: canvas.height });
         this.renderer = new Renderer(canvas);
         this.loopInitialiser();
     }
