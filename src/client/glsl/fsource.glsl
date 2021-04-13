e3a9fa4c253862bb50395b69cf0f91ecc5384824
slooi <s.looi2@outlook.com>
2021-04-13 23:37:57 +1200

    Add sensing to w channel (pheromone when ant has food)

--- src/client/glsl/fsource.glsl
+++ src/client/glsl/fsource.glsl
@@ -5,5 +5,5 @@ varying vec4 v_ColorNSize;
 void main(){
 
 
-	gl_FragColor = vec4(v_ColorNSize.xyz,1.0);
+	gl_FragColor = vec4(vec3(v_ColorNSize.x,v_ColorNSize.y+v_ColorNSize.w,v_ColorNSize.z),1.0);
 }
