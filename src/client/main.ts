c7565834eda7002112951b8045c5f48d881f4a8a
slooi <s.looi2@outlook.com>
2021-04-14 05:20:07 +1200

    temperarially add in game speed functionality

--- src/client/main.ts
+++ src/client/main.ts
@@ -7,3 +7,15 @@ const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
 /* MAIN CODE */
 const game = new Game({ canvas });
+
+window.addEventListener("keydown", (e) => {
+    if (e.key === "w") {
+        game.speed = 10;
+    }
+});
+
+window.addEventListener("keyup", (e) => {
+    if (e.key === "w") {
+        game.speed = 1;
+    }
+});
