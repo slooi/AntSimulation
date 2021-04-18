6303e47d95e354ab91dd1b2994b153b072474c21
slooi <s.looi2@outlook.com>
2021-04-19 09:35:15 +1200

    Add potentially useful FixedLengthArray type

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -5,6 +5,15 @@ Purpose:
 */
 
 /* 
+COPY PASTE
+*/
+type ArrayLengthMutationKeys = "splice" | "push" | "pop" | "shift" | "unshift" | number;
+type ArrayItems<T extends Array<any>> = T extends Array<infer TItems> ? TItems : never;
+type FixedLengthArray<T extends any[]> = Pick<T, Exclude<keyof T, ArrayLengthMutationKeys>> & {
+    [Symbol.iterator]: () => IterableIterator<ArrayItems<T>>;
+};
+
+/* 
 ######## TYPES ##########
 */
 
